961e6b27d9897f8a7e58983d7e9eb26f3fffccbe
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-10-31 16:26:46 +0200

    Refactor theme management to include dark theme support and streamline appTheme function

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
+
+  // Light theme colors
   static const Color tealPrimary = Color(0xFF44BDB6);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF090909);
   static const Color grey = Color(0xFF9F9F9F);
+
+  // Dark theme colors
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color darkText = Color(0xFFFFFFFF);
+  static const Color darkTextSecondary = Color(0xFFB3B3B3);
+  static const Color darkGrey = Color(0xFF616161);
 }
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,10 +2,44 @@ import 'package:flutter/material.dart';
 
 import 'app_colors.dart';
 
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: AppColors.white,
+      primaryColor: AppColors.tealPrimary,
+      colorScheme: const ColorScheme.light(
+        primary: AppColors.tealPrimary,
+        surface: AppColors.white,
+        onSurface: AppColors.black,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.tealPrimary,
+        foregroundColor: Colors.white,
+        elevation: 2,
+      ),
+      useMaterial3: true,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      primaryColor: AppColors.tealPrimary,
+      colorScheme: const ColorScheme.dark(
+        primary: AppColors.tealPrimary,
+        surface: AppColors.darkSurface,
+        onSurface: AppColors.darkText,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.darkSurface,
+        foregroundColor: AppColors.darkText,
+        elevation: 2,
+      ),
+      useMaterial3: true,
+    );
+  }
+}
+
 ThemeData appTheme() {
-  return ThemeData(
-    scaffoldBackgroundColor: AppColors.white,
-    primaryColor: AppColors.tealPrimary,
-    colorScheme: const ColorScheme.light(primary: AppColors.tealPrimary),
-  );
+  return AppTheme.lightTheme;
 }
